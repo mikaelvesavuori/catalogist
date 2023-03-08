b0fb20172c0c3a0c220671c5dc66b57cda1d5255
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2023-03-08 20:57:50 +0100

    fix(): ensure numbers are not getting replace() run on them; add esbuild config; update better test input for complete valid object

--- src/domain/valueObjects/Manifest.ts
+++ src/domain/valueObjects/Manifest.ts
@@ -213,12 +213,14 @@ export class ManifestConstructor {
    * @description String sanitizer utility to cap length and allow only certain characters.
    * @see https://stackoverflow.com/questions/23187013/is-there-a-better-way-to-sanitize-input-with-javascript
    */
-  private sanitizeString(str: string, isValue = false) {
+  private sanitizeString(value: string | number, isValue = false) {
+    if (typeof value === 'number') return value;
     const maxLength = isValue ? 500 : 50;
+
     const regexKeys = new RegExp(/[^a-z0-9@åäöøáéíóúñü\-_]/gim);
     const regexValues = new RegExp(/[^a-z0-9()\[\]\/\:åäöøáéíóúñü\.\s\-_]/gim);
 
-    return (isValue ? str.replace(regexValues, '') : str.replace(regexKeys, ''))
+    return (isValue ? value.replace(regexValues, '') : value.replace(regexKeys, ''))
       .trim()
       .substring(0, maxLength);
   }
